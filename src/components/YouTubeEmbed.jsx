e9c235f206a68a02930f26137888016a0ac5a92f
hubbub-inc <matthewcarrella9@gmail.com>
2024-02-13 16:42:43 -0500

    update logo

--- src/components/YouTubeEmbed.jsx
+++ src/components/YouTubeEmbed.jsx
@@ -4,8 +4,8 @@ import PropTypes from "prop-types";
 const YouTubeEmbed = ({ embedId }) => (
   <div className="video-responsive">
     <iframe
-      width="853"
-      height="480"
+      width="400"
+      height="200"
       src={`https://www.youtube.com/embed/${embedId}`}
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
