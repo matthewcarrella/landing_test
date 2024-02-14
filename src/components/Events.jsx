cf5a2d303262772cd765375985e560db59843a10
hubbub-inc <matthewcarrella9@gmail.com>
2024-02-14 12:58:16 -0500

    update styles

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -15,25 +15,25 @@ export const Events = (props) => {
   };
   return (
     <div>
-      <h2> Basic Carousel Component</h2>
+      <h2 class="slider-h2">Upcoming Events</h2>
       <Slider {...settings}>
         <div>
-          <h3>SLIDE 1</h3>
+          <h3 class="slider-h3">SLIDE 1</h3>
         </div>
         <div>
-          <h3>SLIDE 2</h3>
+          <h3 class="slider-h3">SLIDE 2</h3>
         </div>
         <div>
-          <h3>SLIDE 3</h3>
+          <h3 class="slider-h3">SLIDE 3</h3>
         </div>
         <div>
-          <h3>SLIDE 4</h3>
+          <h3 class="slider-h3">SLIDE 4</h3>
         </div>
         <div>
-          <h3>SLIDE 5</h3>
+          <h3 class="slider-h3">SLIDE 5</h3>
         </div>
         <div>
-          <h3>SLIDE 6</h3>
+          <h3 class="slider-h3">SLIDE 6</h3>
         </div>
       </Slider>
     </div>
