f29eee9286b4b0f38d822cdd9cc0ab31e17fd603
hubbub-inc <matthewcarrella9@gmail.com>
2024-02-14 14:59:00 -0500

    update styles

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -10,13 +10,13 @@ export const Events = (props) => {
   };
   return (
     <Slider style="line-height:0;" {...settings}>
-      <div class="slider-item">
+      <div>
         <h3>1</h3>
       </div>
-      <div class="slider-item">
+      <div>
         <h3>2</h3>
       </div>
-      <div class="slider-item">
+      <div>
         <h3>3</h3>
       </div>
     
