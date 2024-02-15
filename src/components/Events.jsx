92386b0ee104ad1631c8c4882e2da94283da2c1f
hubbub-inc <matthewcarrella9@gmail.com>
2024-02-15 10:48:03 -0500

    update image styles

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,49 +1,13 @@
-import Carousel from 'react-bootstrap/Carousel';
-// export const Events = (props) => {
-//   return ( 
-//     <div style={{ display: 'block', width: 700, padding: 30 }}> 
-//       <h4>React-Bootstrap Carousel Component</h4> 
-//       <Carousel> 
-//         <Carousel.Item interval={1500}> 
-//           <img 
-//             className="d-block w-100"
-// src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122739/2-300x115.png"
-//             alt="Image One"
-//           /> 
-//           <Carousel.Caption> 
-//             <h3>Label for first slide</h3> 
-//             <p>Sample Text for Image One</p> 
-//           </Carousel.Caption> 
-//         </Carousel.Item> 
-//         <Carousel.Item interval={500}> 
-//           <img 
-//             className="d-block w-100"
-// src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122716/1-300x115.png"
-//             alt="Image Two"
-//           /> 
-//           <Carousel.Caption> 
-//             <h3>Label for second slide</h3> 
-//             <p>Sample Text for Image Two</p> 
-//           </Carousel.Caption> 
-//         </Carousel.Item> 
-//       </Carousel> 
-//     </div> 
-//   ); 
-// }
+import AwesomeSlider from 'react-awesome-slider';
+import 'react-awesome-slider/dist/styles.css';
 
 export const Events = (props) => {
   return (
-    <div style={{display: 'block', width: 700, padding: 30}}>
-    <h4>Gallery</h4>
-    <Carousel>
-        { props.data ? props.data.map((d, i) => (
-          <Carousel.Item interval={3000}>
-              <img className="d-block w-100" src={d.largerImage} alt=""/>
-              <Carousel.Caption>
-                  <h3>label for slide</h3>
-              </Carousel.Caption>
-          </Carousel.Item>
-          )) : "..Loading"}
-        </Carousel>
-      </div>);
+  <AwesomeSlider>
+    <div>1</div>
+    <div>2</div>
+    <div>3</div>
+    <div>4</div>
+  </AwesomeSlider>
+);
 };
