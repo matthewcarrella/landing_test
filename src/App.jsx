1eafc2968d0eeaec8edaa7f0a2fd88e1d2333614
hubbub-inc <matthewcarrella9@gmail.com>
2024-02-14 23:24:42 -0500

    update events

--- src/App.jsx
+++ src/App.jsx
@@ -35,6 +35,7 @@ const App = () => {
       <Testimonials data={landingPageData.Testimonials} />
       <Partners data={landingPageData.Partners} />
       <Team data={landingPageData.Team} />
+      <Events data={landingPageData.Gallery}/>
       <Contact data={landingPageData.Contact} />
 
 
