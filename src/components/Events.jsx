4910fc3f939f88ff90106543648da464a3535112
hubbub-inc <matthewcarrella9@gmail.com>
2024-02-20 20:54:07 -0500

    update description

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Slider from "react-slick";
-import Modal from "Modal";
+import Modal from "./Modal";
 import "slick-carousel/slick/slick.css";
 
 import "slick-carousel/slick/slick-theme.css";
