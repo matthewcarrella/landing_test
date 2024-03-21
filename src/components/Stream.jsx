ade2a7c4b8c64ca06442e781b143249a666e7c23
hubbub-inc <matthewcarrella9@gmail.com>
2024-03-21 03:51:56 -0400

    test stream

--- src/components/Stream.jsx
+++ src/components/Stream.jsx
@@ -5,6 +5,5 @@ export const Stream = (props) => {
 		<div>
 		<h3>LiveStream</h3>
 		<iframe src="https://us05web.zoom.us/j/85848442092?pwd=3malKbJRUaWRGSwLo6Kg0QMjXlayd6.1"></iframe>
-		</h3>
 		</div>);
 };
